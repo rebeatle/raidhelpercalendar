20f93b85bb1913d4ea012fb548f1ff2b82579b56
rebeatle <rebeatle2@gmail.com>
2026-03-30 19:49:06 -0500

    solo historicos

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 import requests
+from datetime import datetime
 
-API_KEY = "TU_API_KEY"
-SERVIDORES_GUILDS = ["ID_SERVER_1", "ID_SERVER_2", "ID_SERVER_3"]
 
-def explorar_raids():
-    headers = {"Authorization": API_KEY}
-    
-    print(f"{'FECHA':<20} | {'GUILD':<15} | {'EVENTO':<20} | {'CUPOS'}")
-    print("-" * 70)
-
-    for s_id in SERVIDORES_GUILDS:
-        url = f"https://raid-helper.dev/api/v3/servers/{s_id}/events"
-        res = requests.get(url, headers=headers)
+API_KEY = "HeRPxNmAjLLj9SutMpSpoqx21RVBLZPbmRJ4K8Ic"
+URL_USER = f"https://raid-helper.xyz/api/v4/users/{API_KEY}/events"
+def ver_mi_agenda():
+    try:
+        response = requests.get(URL_USER)
+        raids = response.json()
         
-        if res.status_code == 200:
-            eventos = res.json()
-            for ev in eventos:
-                # Aquí puedes filtrar para que solo muestre las que no te has anotado
-                print(f"{ev['startTimeReadable']:<20} | {ev['serverName']:<15} | {ev['title']:<20} | {ev['signups']}/{ev['capacity']}")
+        # Ordenar por fecha (startTime es unix timestamp)
+        raids_ordenadas = sorted(raids, key=lambda x: x['startTime'])
+        
+        print(f"{'FECHA Y HORA':<18} | {'RAID / EVENTO':<25} | {'LIDER'}")
+        print("-" * 65)
+        
+        for r in raids_ordenadas:
+            # Convertir timestamp a algo legible (renderizamos 24h)
+            fecha = datetime.fromtimestamp(r['startTime']).strftime('%d/%m %H:%M')
+            
+            print(f"{fecha:<18} | {r['title'][:25]:<25} | {r['leaderName']}")
+            
+    except Exception as e:
+        print(f"Error al procesar los datos: {e}")
 
-explorar_raids()
+ver_mi_agenda()
+
+"""https://raid-helper.dev/api/v3/servers/1374406840179232838/events
+https://raid-helper.xyz/api/v4/servers/SERVERID/events
+
+curl --request GET   --url https://raid-helper.xyz/api/v4/servers/SERVERID/events   --header 'Authorization: enter your token here'"""
